f5a435614f1cfb7b73672617f1241386cfe9ef69
manjunathmmath <manjunath.m.math@gmail.com>
2026-07-28 16:54:24 +0530

    changes

--- autoTrade-v4/autotrade.user.js
+++ autoTrade-v4/autotrade.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Groot Bot
 // @namespace    Groot Bot
-// @version      26.74
+// @version      26.75
 // @description  Groot Bot
 // @author       Manjunath
 // @match        https://kite.zerodha.com/*
